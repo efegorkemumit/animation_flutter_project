ba747e668bbe05aa3dd07f838ebc4d3eb2b377a0
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2024-01-18 21:46:59 +0300

    Animaton Login Page 1

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:animation_flutter_project_udemy/animaton/login_animation.dart';
 import 'package:flutter/material.dart';
 
 class AnimatedLoginPage extends StatefulWidget {
@@ -7,10 +8,27 @@ class AnimatedLoginPage extends StatefulWidget {
   State<AnimatedLoginPage> createState() => _AnimatedLoginPageState();
 }
 
-class _AnimatedLoginPageState extends State<AnimatedLoginPage> {
+class _AnimatedLoginPageState extends State<AnimatedLoginPage>  with SingleTickerProviderStateMixin{
+
+  late AnimationController _controller;
+
+  @override
+  void initState(){
+    super.initState();
+    _controller = AnimationController(vsync: this,
+    duration: Duration(seconds: 1),
+      reverseDuration: Duration(milliseconds: 500),
+    );
+  }
+  @override
+  void dispose(){
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return _LoginPage();
+    return _LoginPage(_controller);
   }
 }
 class _LoginPage extends StatelessWidget {
@@ -19,6 +37,14 @@ class _LoginPage extends StatelessWidget {
   
   Color _primaryColor = Color.fromRGBO(255, 194, 60, 1.0);
   Color _secondColor = Color.fromRGBO(16, 7, 32, 1.0);
+
+  late AnimationController _controller;
+  late EnterAnimation _animation;
+
+  _LoginPage(this._controller){
+    _animation = EnterAnimation(_controller);
+    _controller.forward();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,6 +71,31 @@ class _LoginPage extends StatelessWidget {
   }
 
   Widget _avatarWidget(){
-    return  a
+    double _circleD = _deviceHeight * 0.25;
+    return  AnimatedBuilder(
+        animation: _animation.controller,
+        builder: (BuildContext _context, Widget? _widget){
+          return Transform(
+            alignment: Alignment.center,
+              transform: Matrix4.diagonal3Values(
+                  _animation.circleSize.value,
+                  _animation.circleSize.value,
+                  1,
+              ),
+            child: Container(
+              height: _circleD,
+              width: _circleD,
+              decoration: BoxDecoration(
+                color: _secondColor,
+                borderRadius: BorderRadius.circular(600),
+                image: DecorationImage(
+                  image: AssetImage('assets/image/main-profile.png')
+                ),
+              ),
+            ),
+          );
+
+        }
+    );
   }
 }
