a5686005351f65938c9b8a276b839be6f22b1d64
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2024-01-18 21:08:43 +0300

    .

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -10,6 +10,41 @@ class AnimatedLoginPage extends StatefulWidget {
 class _AnimatedLoginPageState extends State<AnimatedLoginPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return _LoginPage();
+  }
+}
+class _LoginPage extends StatelessWidget {
+  var _deviceHeight;
+  var _deviceWidth;
+  
+  Color _primaryColor = Color.fromRGBO(255, 194, 60, 1.0);
+  Color _secondColor = Color.fromRGBO(16, 7, 32, 1.0);
+
+  @override
+  Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: _primaryColor,
+      body: Align(
+        alignment: Alignment.center,
+        child: Container(
+          height: _deviceHeight* 0.60,
+          width: _deviceWidth,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              _avatarWidget(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _avatarWidget(){
+    return 
   }
 }
