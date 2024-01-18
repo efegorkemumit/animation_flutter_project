7f35b5088fe6b4fafa179f236927949f986d8fe1
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2024-01-18 16:50:39 +0300

    first

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AnimatedHomePage extends StatefulWidget {
+  const AnimatedHomePage({super.key});
+
+  @override
+  State<AnimatedHomePage> createState() => _AnimatedHomePageState();
+}
+
+class _AnimatedHomePageState extends State<AnimatedHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AnimatedLoginPage extends StatefulWidget {
+  const AnimatedLoginPage({super.key});
+
+  @override
+  State<AnimatedLoginPage> createState() => _AnimatedLoginPageState();
+}
+
+class _AnimatedLoginPageState extends State<AnimatedLoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
